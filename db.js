fd7d2dcae2c549b1e6f68eb23af28d45c1510db8
ServerSathiNext <mahesh.sugamsofttech@gmail.com>
2025-12-20 18:17:45 +0530

    Fixed DB connection and added environment variables 2

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise'); // <--- MUST have /promise
 require('dotenv').config(); // Make sure to npm install dotenv
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST || 'srv1127.hstgr.io',
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
